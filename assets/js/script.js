ef692764860489576693f8870725a6f220d706f2
Etienne Juffard <etiennejuffard@gmail.com>
2023-07-15 19:10:14 +0200

    fonction factorielle

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,29 +4,95 @@ document.addEventListener('DOMContentLoaded', (e) => {
     let success = {};
     let palindromeForm = document.querySelector('.palindrome-form');
     let palindromeMessage = palindromeForm.querySelector('.palindrome-message');
+    let factorielForm = document.querySelector('.factoriel-form');
+    let factorielMessage = factorielForm.querySelector('.factoriel-message');
+    let factAllNumbers = [];
+
+    factorielForm.querySelector('.factoriel-submit-btn').addEventListener('click', (e) => {
+        error = {}
+        success = {};
+        factAllNumbers = [];
+        let factorielNumber = e.target.closest('form').querySelector('.factoriel-text-input');
+        checkRulesFactoriel(factorielNumber);
+
+        if (Object.entries(error).length > 0) {
+            displayMessage(factorielMessage, 'factoriel-error-display', 'factoriel-success-display', error.factoriel);
+            e.preventDefault();
+            return;
+        }
+        let numberFactoriel = calcFactorielEntry(parseInt(factorielNumber.value));
+        let listNumber = displayAllStepToCalcFactoriel(factorielNumber.value, factAllNumbers);
+        insertFactorielsValuesInModal(numberFactoriel, listNumber);
+    });
 
     palindromeForm.querySelector('.palindrome-submit-btn').addEventListener('click', (e) => {
 
         e.preventDefault();
-        error = {}
+        error = {};
         success = {};
         let palindromeInput = e.target.closest('form').querySelector('#palindrome-text-input');
 
         checkFormRulesPalindrome(palindromeInput);
         if (Object.entries(error).length > 0) {
-            displayMessage('palindrome-error-display', 'palindrome-success-display', error);
+            displayMessage(palindromeMessage, 'palindrome-error-display', 'palindrome-success-display', error.palindrome);
             return;
         }
         palindromeResponse(palindromeInput);
 
         if (success.palindrome) {
-            displayMessage('palindrome-success-display', 'palindrome-error-display', success);
+            displayMessage(palindromeMessage, 'palindrome-success-display', 'palindrome-error-display', success.palindrome);
         }
         if (error.palindrome) {
-            displayMessage('palindrome-error-display', 'palindrome-success-display', error);
+            displayMessage(palindromeMessage, 'palindrome-error-display', 'palindrome-success-display', error.palindrome);
         }
 
     });
+
+    function checkRulesFactoriel(entry) {
+        let value = entry.value.replace(/\s+/g, '');
+        if (value === '') {
+            error.factoriel = 'Attention, vous devez renseigner une valeure';
+        }
+
+        if (/^[0-9\s]*$/.test(value) === false) {
+            error.factoriel = 'Attention, le champ ne peut contenir des nombres';
+        } else {
+            let numberTested = parseInt(value);
+            if (numberTested === 0)
+                error.factoriel = 'Attention, un zéro sera toujours un zéro';
+            if (numberTested >= 50)
+                error.factoriel = 'Attention, on ne va pas dépasser le nombre 50';
+        }
+    }
+
+    function calcFactorielEntry(entry) {
+        if (entry === 0) {
+            return 1;
+        }
+        factAllNumbers.push(entry);
+        return entry * calcFactorielEntry(entry - 1);
+    }
+
+    function displayAllStepToCalcFactoriel(entry, numberArray) {
+        let factorielResultCalc = `${entry}! = `;
+        for (let i = 0; i < numberArray.length; i++) {
+            factorielResultCalc += `${numberArray[i]} * `;
+        }
+        return factorielResultCalc.substring(0, factorielResultCalc.length - 2);
+    }
+
+    function insertFactorielsValuesInModal(factorielResult, factorielOperation)
+    {
+        let modalResult = document.querySelector('.factResult');
+        let modalOperation = document.querySelector('.factOperation');
+        
+        modalResult.innerHTML = '';
+        modalOperation.innerHTML = '';
+
+        modalResult.innerHTML = factorielResult;
+        modalOperation.innerHTML = factorielOperation;
+
+    }
 
     function checkFormRulesPalindrome(entry) {
         let value = entry.value.replace(/\s+/g, '');
@@ -47,16 +113,16 @@ document.addEventListener('DOMContentLoaded', (e) => {
         }
     }
 
-    function displayMessage(inputClassToAdd, inputClass2, valueType) {
-        if (palindromeMessage.classList.contains(inputClassToAdd)) {
-            palindromeMessage.classList.remove(inputClassToAdd);
+    function displayMessage(messageContainer, inputClassToAdd, inputClass2, valueType) {
+        if (messageContainer.classList.contains(inputClassToAdd)) {
+            messageContainer.classList.remove(inputClassToAdd);
         }
-        if (palindromeMessage.classList.contains(inputClass2)) {
-            palindromeMessage.classList.remove(inputClass2);
+        if (messageContainer.classList.contains(inputClass2)) {
+            messageContainer.classList.remove(inputClass2);
         }
 
-        palindromeMessage.innerHTML = valueType.palindrome;
-        palindromeMessage.classList.add(inputClassToAdd);
+        messageContainer.innerHTML = valueType;
+        messageContainer.classList.add(inputClassToAdd);
     }
 
     function palindromeResponse(entry) {
